fb42eabd309616ab61c5ab7241546fc5e9882436
mahadsiyaasi <mahadshankar@gmail.com>
2018-07-07 13:46:20 +0300

    login imrove

--- resources/views/auth/innerlogin.blade.php
+++ resources/views/auth/innerlogin.blade.php
@@ -1,68 +1,114 @@
-<div class="container " style="width: 100%">
+  <style type="text/css">
+    .backimage {
+       background-image: url("{{ asset('loginV1/images/bg-01.jpg') }}") 
+      
+    }
+  </style>
+  <div class="limiter">
+    <div class="container-login100 backimage">
+      <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+          <form class="login100-form validate-form" role="form" method="POST" action="{{ url('/login') }}">
+                      {{ csrf_field() }}
+                      <span class="login100-form-title p-b-49">
+                       <small class="focus-input10">Login</small> <strong style="color:red">Health .</strong><small class="focus-input10">so</small>
+                      </span>
 
-<div class="login-box">
-  <div class="login-logo">
-    <a href="/" class="w3-text-white"><b>Healthso</b><span class="w3-text-black">.so</span></a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body w3-form w3-border w3-card-8 w3-around-large w3-padding">
-    <p class="login-box-msg">Less Time And Efficeient Work</p>
+                      <div class="wrap-input100 validate-input m-b-23 {{ $errors->has('email') ? ' has-error' : '' }}" data-validate = "email is reauired">
+                      @if ($errors->has('email'))
+                                                <span class="help-block danger-alert" style="color: red">
+                                                   {{ $errors->first('email') }}
+                                                </span>
+                                            @endif
+                        <input class="input100" type="email" name="email" placeholder="Type your email">
+                        <span class="focus-input100 large" data-symbol="&#xf206;"></span>
+                         
+                      </div>
+                        
+                      <div class="wrap-input100 validate-input {{ $errors->has('password') ? ' has-error' : '' }}" data-validate="Password is required">
+                       
+                        <input class="input100" type="password" name="password" placeholder="Type your password">
+                        <span class="focus-input100" data-symbol="&#xf190;"></span>
+                         
+                      </div>
+                       @if ($errors->has('password'))
+                                                <span class="help-block">
+                                                    {{ $errors->first('password') }}
+                                                </span>
+                                            @endif
+                      <div class="text-right p-t-8 p-b-31">
+                        <a href="#">
+                          Forgot password?
+                        </a>
+                      </div>
+                      
+                      <div class="container-login100-form-btn">
+                        <div class="wrap-login100-form-btn">
+                          <div class="login100-form-bgbtn"></div>
+                          <button class="login100-form-btn" type="submit">
+                            Login
+                          </button>
+                        </div>
+                      </div>
 
-    <form class="form w3-form" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+                      <div class="txt1 text-center p-t-54 p-b-20">
+                        <span>
+                          Or Sign Up Using
+                        </span>
+                      </div>
 
-    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-    <label for="email" class="control-label">E-Mail Address</label>
-      <div class="form-group has-feedback">
-        <input id="email" type="email" class="form-control" name="email" >
-         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-       
-          @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-     
-      </div> 
+                      <div class="flex-c-m">
+                        <a href="#" class="login100-social-item bg1">
+                          <i class="fa fa-facebook"></i>
+                        </a>
+
+                        <a href="#" class="login100-social-item bg2">
+                          <i class="fa fa-twitter"></i>
+                        </a>
+
+                        <a href="#" class="login100-social-item bg3">
+                          <i class="fa fa-google"></i>
+                        </a>
+                      </div>
+
+                      <div class="flex-col-c p-t-155">
+                        <span class="txt1 p-b-17">
+                          Or Sign Up Using
+                        </span>
+
+                        <a href="#" class="txt2">
+                          Sign Up
+                        </a>
+                      </div>
+        </form>
       </div>
-
-      <div class="form-group has-feedback">
-        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="control-label">Password</label>
-
-        <input id="password" type="password" class="form-control" name="password">
-        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-        
-         @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-      </div> </div>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
-          </div>
-        </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
-
-   
-    <!-- /.social-auth-links -->
-
-    <a href="password/reset">I forgot my password</a><br>
-    <a href="register" class="text-center">Register a new membership</a>
-
+    </div>
   </div>
-  <!-- /.login-box-body -->
-</div>
-</div>
+  
+
+  <div id="dropDownSelect1"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,66 +5,64 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{ url('webicon/cropped.png') }}" type="image/gif" sizes="16x16">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@if(Auth::check()) @yield('title', Route::currentRouteName()) @else @yield('title', __('titles.welcome')) @endif</title>
+     @if (Auth::check())
+              <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+              <link href="{{ asset('css/all.css') }}" rel="stylesheet">   
+              <style type="text/css">
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/all.css') }}" rel="stylesheet">   
-    <style type="text/css">
-      html {
-    overflow-x: hidden;
-}
-@font-face {
-         font-family: 'ChunkFiveRegular';
-         src: url('Chunkfive-webfont.eot');
-         src: url('Chunkfive-webfont.eot?#iefix') format('embedded-opentype'),
-         url('Chunkfive-webfont.woff') format('woff'),
-         url('Chunkfive-webfont.ttf') format('truetype'),
-         url('Chunkfive-webfont.svg#ChunkFiveRegular') format('svg');
-         font-weight: normal;
-         font-style: normal;
-}
-.allback {
-    background: linear-gradient( 135deg, rgba(60, 8, 118, 0.8) 0%, rgba(250, 0, 118, 0.8) 100%);
-}
-.mafont {
-  color: linear-gradient( 135deg, rgba(60, 8, 118, 0.8) 0%, rgba(250, 0, 118, 0.8) 100%);
-}
+                html {
+              overflow-x: hidden;
+            }
+            @font-face {
+                   font-family: 'ChunkFiveRegular';
+                   src: url('Chunkfive-webfont.eot');
+                   src: url('Chunkfive-webfont.eot?#iefix') format('embedded-opentype'),
+                   url('Chunkfive-webfont.woff') format('woff'),
+                   url('Chunkfive-webfont.ttf') format('truetype'),
+                   url('Chunkfive-webfont.svg#ChunkFiveRegular') format('svg');
+                   font-weight: normal;
+                   font-style: normal;
+            }
+            .allback {
+              background: linear-gradient( 135deg, rgba(60, 8, 118, 0.8) 0%, rgba(250, 0, 118, 0.8) 100%);
+            }
+          .mafont {
+            color: linear-gradient( 135deg, rgba(60, 8, 118, 0.8) 0%, rgba(250, 0, 118, 0.8) 100%);
+          }
     </style>
+    @elseif (Auth::guest())    
+     <link rel="stylesheet" type="text/css" href="loginV1/vendor/bootstrap/css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="loginV1/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" type="text/css" href="loginV1/fonts/iconic/css/material-design-iconic-font.min.css">
+       <link rel="stylesheet" type="text/css" href="loginV1/vendor/animate/animate.css">
+      <link rel="stylesheet" type="text/css" href="loginV1/vendor/css-hamburgers/hamburgers.min.css">
+      <link rel="stylesheet" type="text/css" href="loginV1/vendor/animsition/css/animsition.min.css">
+      <link rel="stylesheet" type="text/css" href="loginV1/vendor/select2/select2.min.css">
+        <link rel="stylesheet" type="text/css" href="loginV1/vendor/daterangepicker/daterangepicker.css">
+       <link rel="stylesheet" type="text/css" href="loginV1/css/util.css">
+      <link rel="stylesheet" type="text/css" href="loginV1/css/main.css">
+    @endif   
 </head>
-<body class="hold-transition skin-blue sidebar-mini allback">
+<body class="hold-transition skin-blue sidebar-mini allback" style="height: auto; min-height: 100%;">
     <div id="app">
         <div class="wrapper">
    
      @if (Auth::guest())  
       @include('auth.innerlogin')
     @elseif (Auth::check())
-  <header class="main-header">
-
-    <a href="/" class="logo">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>H</b>S</span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Healthso</b></span>
-    </a>
-
-    <!-- Header Navbar: style can be found in header.less -->
-    <nav class="navbar navbar-static-top">
-      <!-- Sidebar toggle button-->
+    <header class="main-header">
+       <a href="/" class="logo">    
+       <span class="logo-mini"><b>H</b>S</span>
+        <span class="logo-lg"><b>Healthso</b></span>
+       </a>
+     <nav class="navbar navbar-static-top">
       <a href="#" class="sidebar-toggle" data-toggle="push-menu" id="togglemenu" role="button">
         <span class="sr-only">Toggle navigation</span>
       </a>
-    
-   
-      <!-- Navbar Right Menu -->
-      <div class="navbar-custom-menu">
+       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-          <!-- Messages: style can be found in dropdown.less-->
-                    <!-- Notifications: style can be found in dropdown.less -->
-                    <!-- Tasks: style can be found in dropdown.less -->
-          
-          <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="{{ asset('dist/img/avatar.png')}}" class="user-image" alt="User Image">
@@ -82,53 +80,43 @@
                     <li><a href="{{url('/logout')}}"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                   </ul>
           </li>
-          <!-- Control Sidebar Toggle Button -->
-         
         </ul>
       </div>
-
-    </nav>
-
-  </header>
-@yield('content')
- @include("aside.aside")
- @include("confirm.confirm")
- <div  class="specialmodal">
-   
- </div>
-  @endif
-
-
-  
-  </div>
-</div>
- <script src="{{ asset('js/app.js') }}"></script>
-   @push('scripts')   
-    <script src="{{ asset('js/all.js') }}"></script>   
-   <script>
-  $(document).ready(function () {
-      populateCountries("nationality", "city");     
-      $('.sidebar-menu').tree();
-      $('.selectpicker').selectpicker();
-      
-     })
+        </nav>
+      </header>
+    @yield('content')
+     @include("aside.aside")
+     @include("confirm.confirm")
+     <div  class="specialmodal"></div>
+      @endif
+    </div>
+    </div>
     
-</script>
-
-@endpush
- @if (Auth::check())
-@stack('scripts')
-@endif
-
-<script type="text/javascript">
-  //$(".wrapper").css("background","#B03060")
-  //$(".skin-blue .main-header .logo").css("background","#B03060")
-  //$(".main-header .logo").css("background","#B03060")
- // $(".logo").css("background","#B03060")
-  //$(".navbar").css("background","#B03060")
-  
-  
-</script>
+       @push('scripts')    
+          <script src="{{ asset('js/app.js') }}"></script>         
+              <script src="{{ asset('js/all.js') }}"></script>   
+             <script>
+            $(document).ready(function () {
+                populateCountries("nationality", "city");     
+                $('.sidebar-menu').tree();
+                $('.selectpicker').selectpicker();
+               })
+              
+          </script>
+      @endpush
+     @if (Auth::check())
+      @stack('scripts')
+    @else
+          <script src="loginV1/vendor/jquery/jquery-3.2.1.min.js"></script>
+          <script src="loginV1/vendor/animsition/js/animsition.min.js"></script>
+          <script src="loginV1/vendor/bootstrap/js/popper.js"></script>
+          <script src="loginV1/vendor/bootstrap/js/bootstrap.min.js"></script>
+          <script src="loginV1/vendor/select2/select2.min.js"></script>
+          <script src="loginV1/vendor/daterangepicker/moment.min.js"></script>
+          <script src="loginV1/vendor/daterangepicker/daterangepicker.js"></script>
+          <script src="loginV1/vendor/countdowntime/countdowntime.js"></script>
+          <script src="loginV1/js/main.js"></script>
+    @endif
 </body>
 </html>
 
